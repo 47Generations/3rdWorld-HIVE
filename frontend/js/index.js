9ec9711379ba20befd5c7f4efeaf71c0410d4190
47Generations <83317281+47Generations@users.noreply.github.com>
2026-01-21 18:12:39 -0400

    v0.001

---
+++ frontend/js/index.js
@@ -0,0 +1,5 @@
+const HomeButton = document.getElementById("goHome");
+HomeButton.addEventListener('click', function() {
+    // Redirect the user to the desired URL
+    window.location.href = "../index.html";
+});
