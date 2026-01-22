c09c2452674560a343d36d146a99203f98a07a1a
47Generations <83317281+47Generations@users.noreply.github.com>
2026-01-21 21:26:11 -0400

    Almost Version 0.02

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,19 @@
 const HomeButton = document.getElementById("goHome");
 HomeButton.addEventListener('click', function() {
     // Redirect the user to the desired URL
-    window.location.href = "../index.html";
+    window.location.href = "./index.html";
 });
+
+const LogoutButton = document.getElementById("logout");
+LogoutButton.addEventListener('click', function() {
+    // Redirect the user to the desired URL
+    window.location.href = "./splashScreen.html";
+});
+
+
+const WorkButton = document.getElementById("work");
+WorkButton.addEventListener('click', function() {
+    // Redirect the user to the desired URL
+    window.location.href = "./work.html";
+});
+
